7b1385389b898f01376188ed825c87e459ac4523
Privitorta <125799472+Privitorta@users.noreply.github.com>
2025-07-02 00:41:38 +0200

    bacon!

---
+++ cryptingonstructure.py
@@ -0,0 +1,20 @@
+# all'autore piace il bacon
+def privibacon(message):
+    bacon = {
+        'AAAAA': 'A', 'AAAAB': 'B', 'AAABA': 'C', 'AAABB': 'D', 'AABAA': 'E',
+        'AABAB': 'F', 'AABBA': 'G', 'AABBB': 'H', 'ABAAA': 'I', 'ABAAB': 'J',
+        'ABABA': 'K', 'ABABB': 'L', 'ABBAA': 'M', 'ABBAB': 'N', 'ABBBA': 'O',
+        'ABBBB': 'P', 'BAAAA': 'Q', 'BAAAB': 'R', 'BAABA': 'S', 'BAABB': 'T',
+        'BABAA': 'U', 'BABAB': 'V', 'BABBA': 'W', 'BABBB': 'X', 'BBAAA': 'Y',
+        'BBAAB': 'Z'
+    }
+    groups = [message[i:i+5] for i in range(0, len(message), 5)]
+    result = ""
+    for group in groups:
+        if len(group) == 5 and group in bacon:
+            result += bacon[group]
+    return result
+
+message = "AAAABAAAAAAAABAABBBAABBABABAAABAABABAABAABBBABAABBAAAAABAABABAABBBBAAA"
+baconresult = privibacon(message)
+print(f"flag: flag{{{baconresult}}}")
